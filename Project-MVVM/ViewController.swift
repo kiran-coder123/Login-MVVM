5faf8910fc1e44daab4cf03e10e9ae216d8a2bf4
kiran <kiran.sonne@mobisoftinfotech.com>
2021-07-29 14:05:08 +0530

    Initial Commit

---
+++ Project-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-MVVM
+//
+//  Created by Kiran Sonne on 29/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
